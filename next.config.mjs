a9ef6a1cd94f445badfcc92ab3f229ff8886f187
Mukhlis Maulana Al-Fakih <mukhlis.maulanaalf@gmail.com>
2026-03-05 22:02:18 +0700

    update: build readme.md and auto deploy to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "**",
+			},
+		],
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+  output: 'export',
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-      
-    ],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
+    unoptimized: true,
   },
 };
 
